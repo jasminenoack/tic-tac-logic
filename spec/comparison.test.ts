ecbf39ee7bae1b1f8894b0f90a0e131d8dcc856e
Jasmine Noack <jasminenoack@gmail.com>
2017-09-20 21:17:55 -0400

    Add the comparison method

--- spec/comparison.test.ts
+++ spec/comparison.test.ts
@@ -18,33 +18,33 @@ describe("spots match", () => {
         undefinedSpotTwo = new Spot();
     });
 
-    xit("should think 2 spots with Os are equal", () => {
+    it("should think 2 spots with Os are equal", () => {
         expect(ComparisonManager.compareTwo(oSpotOne, oSpotTwo)).toBeTruthy();
     });
 
-    xit("should think 2 spots with Xs are equal", () => {
+    it("should think 2 spots with Xs are equal", () => {
         expect(ComparisonManager.compareTwo(xSpotOne, xSpotTwo)).toBeTruthy();
     });
 
-    xit("should think 2 spots that are not known are not equal", () => {
+    it("should think 2 spots that are not known are not equal", () => {
         expect(ComparisonManager.compareTwo(undefinedSpotOne, undefinedSpotTwo)).toBeFalsy();
     });
 
-    xit("should think an X spot and an O spot are not equal", () => {
+    it("should think an X spot and an O spot are not equal", () => {
         expect(ComparisonManager.compareTwo(xSpotOne, oSpotOne)).toBeFalsy();
         expect(ComparisonManager.compareTwo(xSpotTwo, oSpotTwo)).toBeFalsy();
         expect(ComparisonManager.compareTwo(xSpotOne, oSpotTwo)).toBeFalsy();
         expect(ComparisonManager.compareTwo(xSpotTwo, oSpotOne)).toBeFalsy();
     });
 
-    xit("should think an X spot and an undefined spot are not equal", () => {
+    it("should think an X spot and an undefined spot are not equal", () => {
         expect(ComparisonManager.compareTwo(xSpotOne, undefinedSpotOne)).toBeFalsy();
         expect(ComparisonManager.compareTwo(xSpotOne, undefinedSpotTwo)).toBeFalsy();
         expect(ComparisonManager.compareTwo(xSpotTwo, undefinedSpotOne)).toBeFalsy();
         expect(ComparisonManager.compareTwo(xSpotTwo, undefinedSpotTwo)).toBeFalsy();
     });
 
-    xit("should think an O spot and an undefined spot are not equal", () => {
+    it("should think an O spot and an undefined spot are not equal", () => {
         expect(ComparisonManager.compareTwo(oSpotOne, undefinedSpotOne)).toBeFalsy();
         expect(ComparisonManager.compareTwo(oSpotOne, undefinedSpotTwo)).toBeFalsy();
         expect(ComparisonManager.compareTwo(oSpotTwo, undefinedSpotOne)).toBeFalsy();
--- src/comparisonManager.ts
+++ src/comparisonManager.ts
@@ -2,6 +2,8 @@ import { Spot } from "./spot";
 
 export class ComparisonManager {
     public static compareTwo(firstSpot: Spot, secondSpot: Spot) {
-
+        const firstValue = firstSpot.get();
+        const secondValue = secondSpot.get();
+        return !!(firstValue && secondValue && firstValue === secondValue);
     }
 }
